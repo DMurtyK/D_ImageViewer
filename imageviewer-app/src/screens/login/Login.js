c53bb840fcd25c9e9e79ed8328d8b7f0c3cff9a7
Deepa Murty Tekumalla <naradeepu@gmail.com>
2019-05-15 00:28:07 +0200

    Added Login Component

---
+++ imageviewer-app/src/screens/login/Login.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Login.css';
+
+class Login extends Component{
+
+    render(){
+        return(
+
+
+            <div>
+            Image Viewer
+            </div>
+        )
+    }
+}
+
+export default Login;
