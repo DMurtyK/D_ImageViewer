85bc5e6af657c329f3514eff756fea3d4ef013f1
Deepa Murty Tekumalla <naradeepu@gmail.com>
2019-05-15 01:15:16 +0200

    Added the logo as text in the header component

--- imageviewer-app/src/screens/login/Login.js
+++ imageviewer-app/src/screens/login/Login.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import './Login.css';
+import Header from '../../common/header/Header.js';
 
 class Login extends Component{
 
     render(){
         return(
-
-
             <div>
-            Image Viewer
+            <Header />
+            
             </div>
+            
+
+            
         )
     }
 }
