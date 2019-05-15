8d3b0b284368c2f933eb9186398e2f1bdaa89e52
Deepa Murty Tekumalla <naradeepu@gmail.com>
2019-05-15 02:39:43 +0200

    card displayed horizontally in center of the page

--- imageviewer-app/src/screens/login/Login.js
+++ imageviewer-app/src/screens/login/Login.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 import './Login.css';
 import Header from '../../common/header/Header.js';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
-class Login extends Component{
+class Login extends Component {
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
-            <Header />
-            
-            </div>
-            
+                <Header />
+                <div className="cardStyle">
 
-            
+                <Card >
+                    <CardContent>
+
+                        <span>LOGIN</span>
+
+                    </CardContent>
+
+                </Card>
+
+                
+                
+                </div>
+
+                
+
+            </div>
+
+
+
         )
     }
 }
