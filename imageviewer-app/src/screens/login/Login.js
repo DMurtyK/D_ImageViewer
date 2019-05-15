3af8dd440dded7765f7775dbe31cb8b6eb3e420c
Deepa Murty Tekumalla <naradeepu@gmail.com>
2019-05-15 02:56:04 +0200

    display two text fields - one for username and the other for password

--- imageviewer-app/src/screens/login/Login.js
+++ imageviewer-app/src/screens/login/Login.js
@@ -5,6 +5,10 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+
 class Login extends Component {
 
     render() {
@@ -17,6 +21,15 @@ class Login extends Component {
                     <CardContent>
 
                         <Typography variant ="h6">LOGIN</Typography>
+                        <FormControl required>
+                            <InputLabel htmlFor="username">Username</InputLabel>
+                            <Input id="username" type="text" />
+                        </FormControl><br />
+
+                         <FormControl required>
+                            <InputLabel htmlFor="password">Password</InputLabel>
+                            <Input id="password" type="password" />
+                        </FormControl>
 
                     </CardContent>
 
